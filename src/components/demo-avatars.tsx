53e4d21007b80118d83f83676d3c2bfa4384df59
Gregory Pratt <gregory.pratt@anaplan.com>
2022-03-01 14:40:55 +0000

    update deps

--- src/components/demo-avatars.tsx
+++ src/components/demo-avatars.tsx
@@ -9,47 +9,47 @@ export class Avatars {
       <section class="u-window-box-super">
         <h2 class="c-heading">Avatars</h2>
         <div class="c-avatar c-avatar u-super">
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/3B6DC6/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/3B6DC6/FFF" />
         </div>
         <div class="c-avatar c-avatar u-xlarge">
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/3B6DC6/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/3B6DC6/FFF" />
         </div>
         <div class="c-avatar c-avatar u-large">
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/3B6DC6/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/3B6DC6/FFF" />
         </div>
         <div class="c-avatar">
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/3B6DC6/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/3B6DC6/FFF" />
         </div>
         <div class="c-avatar c-avatar u-small">
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/3B6DC6/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/3B6DC6/FFF" />
         </div>
         <div class="c-avatar c-avatar u-xsmall">
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/3B6DC6/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/3B6DC6/FFF" />
         </div>
         <br />
         <div class="c-avatar c-avatar u-super">
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/3B6DC6/FFF" />
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/BADA55/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/3B6DC6/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/BADA55/FFF" />
         </div>
         <div class="c-avatar c-avatar u-xlarge">
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/3B6DC6/FFF" />
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/BADA55/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/3B6DC6/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/BADA55/FFF" />
         </div>
         <div class="c-avatar c-avatar u-large">
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/3B6DC6/FFF" />
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/BADA55/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/3B6DC6/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/BADA55/FFF" />
         </div>
         <div class="c-avatar">
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/3B6DC6/FFF" />
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/BADA55/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/3B6DC6/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/BADA55/FFF" />
         </div>
         <div class="c-avatar c-avatar u-small">
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/3B6DC6/FFF" />
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/BADA55/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/3B6DC6/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/BADA55/FFF" />
         </div>
         <div class="c-avatar c-avatar u-xsmall">
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/3B6DC6/FFF" />
-          <img class="c-avatar__img" alt="placeholder" src="http://placehold.it/1024x963/BADA55/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/3B6DC6/FFF" />
+          <img class="c-avatar__img" alt="placeholder" src="http://via.placeholder.com/1024x963/BADA55/FFF" />
         </div>
         <br />
         <div class="c-avatar c-avatar u-super" data-text="GP" />
--- src/components/demo-images.tsx
+++ src/components/demo-images.tsx
@@ -8,7 +8,7 @@ export class Images {
     return (
       <section class="u-window-box-super">
         <h2 class="c-heading">Images</h2>
-        <img class="o-image" src="//placehold.it/1024x963/3B6DC6/FFF" alt="random image" />
+        <img class="o-image" src="//via.placeholder.com/1024x963/3B6DC6/FFF" alt="random image" />
       </section>
     );
   }
